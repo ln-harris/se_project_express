b7d1360585b3c7c265c6d030abbcf9b54dbe6d43
LindseyH <linsnh24@gmail.com>
2026-08-12 19:40:19 -0700

    Add signup and signin authentication

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
+const { login, createUser } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
+
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
