057a6e82531c50b18a9e68cc9c216d0b683ccb65
LindseyH <linsnh24@gmail.com>
2026-08-14 13:50:47 -0700

    Enable CORS for API requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const mainRouter = require("./routes/index");
 
 const app = express();
@@ -12,6 +13,7 @@ mongoose
   })
   .catch(console.error);
 
+app.use(cors());
 app.use(express.json());
 app.use("/", mainRouter);
 
